b59998cb09dfc50d0eae5925a59145c4f3b5a432
VMolinov <molinov@list.ru>
2021-01-24 22:11:25 +0300

    Done

--- src/lesson6/Client.java
+++ src/lesson6/Client.java
@@ -1,4 +1,44 @@
 package lesson6;
 
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Client {
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("localhost", 8081);
+        Scanner scanner = new Scanner(System.in);
+        DataInputStream in = new DataInputStream(socket.getInputStream());
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String inputMessage = null;
+                do {
+                    try {
+                        inputMessage = in.readUTF();
+                        System.out.println(inputMessage);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } while (!"\\close".equals(inputMessage));
+            }
+        }).start();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String outputMessage = null;
+                do {
+                    try {
+                        outputMessage = scanner.nextLine();
+                        out.writeUTF(outputMessage);
+                        out.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } while (!"\\close".equals(outputMessage));
+            }
+        }).start();
+    }
 }
--- src/lesson6/Server.java
+++ src/lesson6/Server.java
@@ -1,4 +1,49 @@
 package lesson6;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class Server {
+
+    public static void main(String[] args) throws IOException {
+        try (ServerSocket serverSocket = new ServerSocket(8081);) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Server socket created, waiting response");
+            Socket socket = serverSocket.accept();
+            System.out.println("Client connected");
+            DataInputStream in = new DataInputStream(socket.getInputStream());
+            DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String inputMessage = null;
+                    do {
+                        try {
+                            inputMessage = in.readUTF();
+                            System.out.println(inputMessage);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } while (!"\\close".equals(inputMessage));
+                }
+            }).start();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    String outputMessage = null;
+                    do {
+                        try {
+                            outputMessage = scanner.nextLine();
+                            out.writeUTF(outputMessage);
+                            out.flush();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } while (!"\\close".equals(outputMessage));
+                }
+            }).start();
+        }
+    }
 }
