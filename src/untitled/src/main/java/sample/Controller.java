0d7ec673774edd0ac7e4117b40f8ad107d005626
VMolinov <molinov@list.ru>
2021-01-26 22:03:32 +0300

    FXML

--- src/untitled/src/main/java/sample/Controller.java
+++ src/untitled/src/main/java/sample/Controller.java
@@ -1,4 +1,34 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class Controller {
+
+    @FXML
+    private TextArea chat;
+
+    @FXML
+    private TextArea users;
+
+    @FXML
+    private TextField input;
+
+    @FXML
+    private Button inputMessage;
+
+    @FXML
+    private Button exit;
+
+    @FXML
+    void addMessage() {
+
+    }
+
+    @FXML
+    void exit() {
+
+    }
 }
